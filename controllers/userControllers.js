7676ea5584504061969c8619a03d1c6d59b742bf
abhishek lohani <alohani1308@gmail.com>
2023-03-20 00:06:41 +0530

    User registration and password hashing for contact managment application

---
+++ controllers/userControllers.js
@@ -0,0 +1,56 @@
+const User=require("../models/userModel");
+const bcrypt=require("bcrypt");
+
+const registerUser=async(req,res)=>{
+    const { username,email,password }=req.body;
+    //to check all fields are filled 
+    if(!email || !username || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    //To check is given email registered previously
+    const userAvailable=await User.findOne({ email });
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+    //Hash Password
+    const hashedPassword=await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ",hashedPassword);
+
+    //filling data into database
+    const user=await User.create({
+        username,
+        email,
+        password:hashedPassword
+    });
+
+    console.log(`User created ${user}`);
+    if(user){
+        res.status(201).json({ _id:user.id, email:user.email });
+    }
+    else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+
+    res.json({ message: "Register the User" });
+}
+
+const loginUser=async(req,res)=>{
+    res.json({ message:"Login User"});
+}
+
+const currentUser=async(req,res)=>{
+    res.json({ message: "Current User Information"  })
+}
+
+module.exports={ registerUser,loginUser,currentUser }
+
+// {
+//     "username":"lohani",
+//     "email":"alohani1308@gmail.com",
+//     "password":"1234567890"
+
+// }
