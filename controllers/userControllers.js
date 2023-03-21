87cc83f7f8f3f662680f0a34267a2a65c89507a3
abhishek lohani <alohani1308@gmail.com>
2023-03-21 13:07:47 +0530

    JWT token added for contact managment application

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,6 @@
 const User=require("../models/userModel");
 const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken");
 
 const registerUser=async(req,res)=>{
     const { username,email,password }=req.body;
@@ -39,11 +40,33 @@ const registerUser=async(req,res)=>{
 }
 
 const loginUser=async(req,res)=>{
-    res.json({ message:"Login User"});
+    const { email,password }=req.body;
+    if( !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory !");
+    }
+    const user=await User.findOne({ email });
+    //console.log(user);
+    if(user && (await bcrypt.compare(password,user.password))){
+        const accessToken=jwt.sign({
+            user:{
+                username:user.username,
+                email:user.email,
+                id:user.id
+            },
+        },process.env.ACCESS_TOKEN_KEY,
+        { expiresIn: "1d" }
+        )
+        res.status(200).json({ accessToken });
+    }
+    else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
 }
 
 const currentUser=async(req,res)=>{
-    res.json({ message: "Current User Information"  })
+    res.json(req.user)
 }
 
 module.exports={ registerUser,loginUser,currentUser }
